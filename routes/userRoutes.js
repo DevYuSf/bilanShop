e0ed00e043c2fbee484e3f9c2e9fdd0ee2ad9f46
DevYuSf <yusufisxq@gmail.com>
2024-12-18 22:15:18 +0300

    second

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express  from "express";
-import { createUser, getUserById, getUsers, updateUser } from "../controller/userController.js";
+import { createUser, getUserById, getUsers, login, updateUser } from "../controller/userController.js";
 
 const router = express.Router();
 
 router.route('/').get(getUsers).post(createUser);
+router.route('/login').post(login);
 
 router.route('/:id').get(getUserById).put(updateUser);
 
